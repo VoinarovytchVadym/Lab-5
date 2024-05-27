9e3bc8a15920b97bd606c8faca7d1f25ce5c9483
VoinarovytchVadym <ipz224_vvv@student.ztu.edu.ua>
2024-05-27 15:21:51 +0300

    LifeCycleHooks were added

---
+++ HTML/LifeCycleHooks/LifeCycleHooks.cs
@@ -0,0 +1,16 @@
+﻿using HTML.Nodes;
+
+namespace HTML.LifeCycleHooks;
+
+public abstract class LifeCycleHooks
+{
+    public virtual void OnCreated(LightElementNode node){}
+    public virtual void OnClassesApplied(LightElementNode node){}
+
+    public void Run(LightElementNode node)
+    {
+        OnCreated(node);
+        if (node.Classes.Count > 0)
+            OnClassesApplied(node);
+    }
+}
